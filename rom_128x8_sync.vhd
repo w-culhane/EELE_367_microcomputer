0abd62e5342087220c9286c5b68130a820cdfab2
William Culhane <will@culhane.top>
2021-03-05 21:52:06 -0700

    Comment out opcodes until they are implemented

--- rom_128x8_sync.vhd
+++ rom_128x8_sync.vhd
@@ -13,33 +13,33 @@ architecture rom_128x8_sync_arch of rom_128x8_sync is
   type ROM_type is array(0 to 127) of std_logic_vector(7 downto 0);
 
   -- Loading/storing
-  constant LDA_IMM : std_logic_vector(7 downto 0) := x"86";
-  constant LDB_IMM : std_logic_vector(7 downto 0) := x"88";
-  constant LDA_DIR : std_logic_vector(7 downto 0) := x"87";
-  constant LDB_DIR : std_logic_vector(7 downto 0) := x"89";
-  constant STA_DIR : std_logic_vector(7 downto 0) := x"96";
-  constant STB_DIR : std_logic_vector(7 downto 0) := x"97";
+  -- constant LDA_IMM : std_logic_vector(7 downto 0) := x"86";
+  -- constant LDB_IMM : std_logic_vector(7 downto 0) := x"88";
+  -- constant LDA_DIR : std_logic_vector(7 downto 0) := x"87";
+  -- constant LDB_DIR : std_logic_vector(7 downto 0) := x"89";
+  -- constant STA_DIR : std_logic_vector(7 downto 0) := x"96";
+  -- constant STB_DIR : std_logic_vector(7 downto 0) := x"97";
 
   -- Data manipulation
-  constant ADD_AB : std_logic_vector(7 downto 0) := x"42";
-  constant SUB_AB : std_logic_vector(7 downto 0) := x"43";
-  constant AND_AB : std_logic_vector(7 downto 0) := x"44";
-  constant OR_AB  : std_logic_vector(7 downto 0) := x"45";
-  constant INCA   : std_logic_vector(7 downto 0) := x"46";
-  constant INCB   : std_logic_vector(7 downto 0) := x"47";
-  constant DECA   : std_logic_vector(7 downto 0) := x"48";
-  constant DECB   : std_logic_vector(7 downto 0) := x"49";
+  -- constant ADD_AB : std_logic_vector(7 downto 0) := x"42";
+  -- constant SUB_AB : std_logic_vector(7 downto 0) := x"43";
+  -- constant AND_AB : std_logic_vector(7 downto 0) := x"44";
+  -- constant OR_AB  : std_logic_vector(7 downto 0) := x"45";
+  -- constant INCA   : std_logic_vector(7 downto 0) := x"46";
+  -- constant INCB   : std_logic_vector(7 downto 0) := x"47";
+  -- constant DECA   : std_logic_vector(7 downto 0) := x"48";
+  -- constant DECB   : std_logic_vector(7 downto 0) := x"49";
 
   -- Control flow/branching
-  constant BRA : std_logic_vector(7 downto 0) := x"20";
-  constant BMI : std_logic_vector(7 downto 0) := x"21";
-  constant BPL : std_logic_vector(7 downto 0) := x"22";
-  constant BEQ : std_logic_vector(7 downto 0) := x"23";
-  constant BNE : std_logic_vector(7 downto 0) := x"24";
-  constant BVS : std_logic_vector(7 downto 0) := x"25";
-  constant BVC : std_logic_vector(7 downto 0) := x"26";
-  constant BCS : std_logic_vector(7 downto 0) := x"27";
-  constant BCC : std_logic_vector(7 downto 0) := x"28";
+  -- constant BRA : std_logic_vector(7 downto 0) := x"20";
+  -- constant BMI : std_logic_vector(7 downto 0) := x"21";
+  -- constant BPL : std_logic_vector(7 downto 0) := x"22";
+  -- constant BEQ : std_logic_vector(7 downto 0) := x"23";
+  -- constant BNE : std_logic_vector(7 downto 0) := x"24";
+  -- constant BVS : std_logic_vector(7 downto 0) := x"25";
+  -- constant BVC : std_logic_vector(7 downto 0) := x"26";
+  -- constant BCS : std_logic_vector(7 downto 0) := x"27";
+  -- constant BCC : std_logic_vector(7 downto 0) := x"28";
 
   constant ROM : ROM_type := (0      => "00000000",
                               1      => "00000000",
