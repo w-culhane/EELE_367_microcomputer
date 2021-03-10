57b5614a8fdc34ab642f5335536745f26c7c03ed
William Culhane <will@culhane.top>
2021-03-10 14:46:12 -0700

    Add CPU declaration to computer

--- computer.vhd
+++ computer.vhd
@@ -42,6 +42,17 @@ end entity;
 
 architecture computer_arch of computer is
 
+
+  component cpu
+    port (clock : in std_logic;
+          reset : in std_logic;
+
+          write       : out std_logic;
+          address     : out std_logic_vector(7 downto 0);
+          from_memory : in  std_logic_vector(7 downto 0);
+          to_memory   : out std_logic_vector(7 downto 0));
+  end component;
+
   component memory
     port (clock : in std_logic;
           reset : in std_logic;
@@ -86,50 +97,25 @@ architecture computer_arch of computer is
           port_out_15 : out std_logic_vector(7 downto 0));
   end component;
 
-  signal write                      : std_logic;
-  signal address, data_in, data_out : std_logic_vector(7 downto 0);
+  signal write                    : std_logic;
+  signal address, mem_in, mem_out : std_logic_vector(7 downto 0);
 
 begin
 
-  MEM : memory port map(clock => clock, reset => reset,
+  CPU0 : cpu port map (clock, reset, write, address, mem_out, mem_in);
 
-                        address  => address,   -- TODO
-                        write    => write,     -- TODO
-                        data_in  => data_in,   -- TODO
-                        data_out => data_out,  -- TODO
+  MEM0 : memory port map(clock, reset,
 
-                        port_in_00 => port_in_00,
-                        port_in_01 => port_in_01,
-                        port_in_02 => port_in_02,
-                        port_in_03 => port_in_03,
-                        port_in_04 => port_in_04,
-                        port_in_05 => port_in_05,
-                        port_in_06 => port_in_06,
-                        port_in_07 => port_in_07,
-                        port_in_08 => port_in_08,
-                        port_in_09 => port_in_09,
-                        port_in_10 => port_in_10,
-                        port_in_11 => port_in_11,
-                        port_in_12 => port_in_12,
-                        port_in_13 => port_in_13,
-                        port_in_14 => port_in_14,
-                        port_in_15 => port_in_15,
+                         address, write, mem_in, mem_out,
 
-                        port_out_00 => port_out_00,
-                        port_out_01 => port_out_01,
-                        port_out_02 => port_out_02,
-                        port_out_03 => port_out_03,
-                        port_out_04 => port_out_04,
-                        port_out_05 => port_out_05,
-                        port_out_06 => port_out_06,
-                        port_out_07 => port_out_07,
-                        port_out_08 => port_out_08,
-                        port_out_09 => port_out_09,
-                        port_out_10 => port_out_10,
-                        port_out_11 => port_out_11,
-                        port_out_12 => port_out_12,
-                        port_out_13 => port_out_13,
-                        port_out_14 => port_out_14,
-                        port_out_15 => port_out_15);
+                         port_in_00, port_in_01, port_in_02, port_in_03,
+                         port_in_04, port_in_05, port_in_06, port_in_07,
+                         port_in_08, port_in_09, port_in_10, port_in_11,
+                         port_in_12, port_in_13, port_in_14, port_in_15,
+
+                         port_out_00, port_out_01, port_out_02, port_out_03,
+                         port_out_04, port_out_05, port_out_06, port_out_07,
+                         port_out_08, port_out_09, port_out_10, port_out_11,
+                         port_out_12, port_out_13, port_out_14, port_out_15);
 
 end architecture;
